4210140cf5619608b0883c2417170bb996edb487
Jakub Brzegowski <symfiz@gmail.com>
2018-02-24 13:36:13 +0100

    Added prettier, tslint-config-prettier, husky, lint-staged.

--- src/server/config.ts
+++ src/server/config.ts
@@ -1,36 +1,49 @@
-export const dbPasswordSalt = 'asw';
-export const jwtSalt = 'asdf';
+export const dbPasswordSalt = "asw";
+export const jwtSalt = "asdf";
 
-export const alvisProjectsFilesDir = './static/alvis-project/';
+export const alvisProjectsFilesDir = "./static/alvis-project/";
 
 export const dbConfig = {
-    development: {
-        username: 'alviseditor',
-        password: 'alviseditor',
-        database: 'alviswebeditor',
-        options: {
-            host: '/var/run/postgresql',
-            // host: 'localhost',
-            dialect: 'postgres',
-            pool: {
-                max: 5,
-                min: 0,
-                idle: 10000
-            }
-        }
-    },
-    production: {
-        username: 'postgres',
-        password: '',
-        database: '',
-        options: {
-            host: null,
-            dialect: 'alviswebeditor',
-            pool: {
-                max: 5,
-                min: 0,
-                idle: 10000
-            }
-        }
+  developmentLinux: {
+    username: "alviseditor",
+    password: "alviseditor",
+    database: "alviswebeditor",
+    options: {
+      host: "/var/run/postgresql",
+      dialect: "postgres",
+      pool: {
+        max: 5,
+        min: 0,
+        idle: 10000
+      }
     }
-}
+  },
+  development: {
+    username: "postgres",
+    password: "password",
+    database: "alviswebeditor",
+    options: {
+      host: "localhost",
+      dialect: "postgres",
+      pool: {
+        max: 5,
+        min: 0,
+        idle: 10000
+      }
+    }
+  },
+  production: {
+    username: "postgres",
+    password: "",
+    database: "",
+    options: {
+      host: null,
+      dialect: "alviswebeditor",
+      pool: {
+        max: 5,
+        min: 0,
+        idle: 10000
+      }
+    }
+  }
+};
